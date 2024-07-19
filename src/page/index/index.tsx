7adc63e7a0d1790bcd1f7c742581d87e84f848d6
velpro <1326143109@qq.com>
2024-07-19 16:41:01 +0800

    xx

--- src/page/index/index.tsx
+++ src/page/index/index.tsx
@@ -7,6 +7,9 @@ export const Index = () => {
       console.log(res)
     })
   })
+
+  //TODO下面两种写法一样
+  const otherChildren = () => <LayoutContainer children={<div>首页</div>}></LayoutContainer>
   return (
     <LayoutContainer>
       <div>首页</div>
